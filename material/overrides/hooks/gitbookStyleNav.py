36b7afcea33ed05d13b2929d9e93675459f7b204
Evariste-xu <3355340673@qq.com>
2025-02-08 18:13:38 +0800

    vault backup: 2025-02-08 18:13:38

--- material/overrides/hooks/gitbookStyleNav.py
+++ material/overrides/hooks/gitbookStyleNav.py
@@ -2,3 +2,10 @@ import os
 import re
 
 from glob import iglob
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.structure.pages import Page
+from urllib.parse import urlencode, urlparse
+
+def on_page_content(context: dict, page: Page, config: MkDocsConfig, nav: Navigation):
+
+
